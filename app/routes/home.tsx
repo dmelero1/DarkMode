a56dce771cd2365f4d1ca388ccb19f350d0f76f9
dmelero1 <danielmm1103@gmail.com>
2025-02-20 14:07:55 +0100

    Uso componentes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router";
+import ThemeToggle from "~/components/ThemeToggle";
+import Card1 from "~/components/Card1";
+import Header from "~/components/Header";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="flex items-center justify-center pb-4">
+      <div className="flex-1 flex flex-col ml-15 mr-15 gap-16 min-h-0">
+        <Header />
+        <Card1 />
+      </div>
+    </main>
+  )
 }
