fecee75ee52d40e3c9e343e13a1238fbc02234c9
dmelero1 <danielmm1103@gmail.com>
2025-02-24 13:20:05 +0100

    Uso Variables + Responsivo

--- app/components/Card1.tsx
+++ app/components/Card1.tsx
@@ -1,61 +1,123 @@
 import chica from "../assets/chica.png";
 
 const profiles = [
-    { id: 1, name: "Rosa", age: 35, education:"UCLM", location:"Toledo", job:"Family Physician", hobbies:"Hiking and Dancing"}
+  {
+    id: 1,
+    name: "Rosa",
+    age: 35,
+    education: "UCLM",
+    location: "Toledo",
+    job: "Web Developer",
+    hobbies: "Football and Dancing",
+  },
 ];
 
 function Card1() {
   return (
-    <div className="grid grid-cols-3 grid-rows-2 gap-6 p-8">
-      <div className="bg-blue-200 dark:bg-blue-400 rounded-lg shadow-lg border border-gray-800 overflow-hidden row-span-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-0">
+      <div className="p-5 bg-[var(--color-card-bg-light)] dark:bg-[var(--color-card-bg-dark)] rounded-lg shadow-lg border overflow-hidden row-span-2">
         <img className="w-full" src={chica} alt="Imagen" />
-        <div className="p-5 flex justify-between items-center">
-          <h2>{profiles[0].name}</h2>
-          <h2>{profiles[0].age}</h2>
+        <div className="p-4 flex justify-between items-center">
+          <h2 className="font-bold text-[var(--color-card-h2-light)] dark:text-[var(--color-card-h2-dark)]">
+            {profiles[0].name}
+          </h2>
+          <h2 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-h2-dark)]">
+            {profiles[0].age}
+          </h2>
         </div>
-        <div className="p-5 flex justify-between items-center">
-          <h2>Education:</h2>
-          <h2>{profiles[0].education}</h2>
+        <div className="p-4 flex justify-between items-center">
+          <h2 className="font-bold text-[var(--color-card-h2-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            Education:
+          </h2>
+          <h2 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            {profiles[0].education}
+          </h2>
         </div>
-        <div className="p-5 flex justify-between items-center">
-          <h2>Location:</h2>
-          <h2>{profiles[0].location}</h2>
+        <div className="p-4 flex justify-between items-center">
+          <h2 className="font-bold text-[var(--color-card-h2-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            Location:
+          </h2>
+          <h2 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            {profiles[0].location}
+          </h2>
         </div>
-        <div className="p-5 flex justify-between items-center">
-          <h2>Job:</h2>
-          <h2>{profiles[0].job}</h2>
+        <div className="p-4 flex justify-between items-center">
+          <h2 className="font-bold text-[var(--color-card-h2-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            Job:
+          </h2>
+          <h2 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            {profiles[0].job}
+          </h2>
         </div>
-        <div className="p-5 flex justify-between items-center">
-          <h2>Hobbies:</h2>
-          <h2>{profiles[0].hobbies}</h2>
+        <div className="p-4 flex justify-between items-center">
+          <h2 className="font-bold text-[var(--color-card-h2-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            Hobbies:
+          </h2>
+          <h2 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+            {profiles[0].hobbies}
+          </h2>
         </div>
       </div>
 
       {/* Cuatro cajas pequeñas */}
-      <div className="bg-blue-200 dark:bg-blue-400 p-8 rounded-lg shadow-md">
-        <h2 className="font-bold text-xl mb-3">Bio</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore alias velit nam aperiam corrupti nobis minus tenetur cumque fugiat reprehenderit sint asperiores suscipit officiis ab, amet atque, praesentium ipsam! Doloribus.</p>
+      <div className="bg-[var(--color-card-bg-light)] dark:bg-[var(--color-card-bg-dark)] p-8 rounded-lg shadow-md">
+        <h2 className="font-bold text-3xl mb-3 text-[var(--color-card-h2-light)] dark:text-[var(--color-card-h2-dark)]">
+          Bio
+        </h2>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Imperdiet mauris enim, vel condimentum risus eget rhoncus, vitae. Viverra pulvinar cras eu ac sed nunc dui. Risus pharetra, lacus interdum quam. Egestas aliquam dui at proin. Amet, pharetra, scelerisque molestie nibh porttitor sem malesuada cursus eget. Orci.
+        </p>
       </div>
-      <div className="bg-blue-200 dark:bg-blue-400 p-8 rounded-lg shadow-md">
-      <h2 className="font-bold text-xl mb-3">Personality</h2>
-        <h4>- Handworking</h4>
-        <h4>- Smart</h4>
-        <h4>- Expressive</h4>
-        <h4>- Thoughful</h4>
+      <div className="bg-[var(--color-card-bg-light)] dark:bg-[var(--color-card-bg-dark)] p-8 rounded-lg shadow-md">
+        <h2 className="font-bold text-3xl mb-3 text-[var(--color-card-h2-light)] dark:text-[var(--color-card-h2-dark)]">
+          Personality
+        </h2>
+        <h3 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Hardworking
+        </h3>
+        <h3 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Smart
+        </h3>
+        <h3 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Expressive
+        </h3>
+        <h3 className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Thoughtful
+        </h3>
       </div>
-      <div className="bg-blue-200 dark:bg-blue-400 p-8 rounded-lg shadow-md">
-        <h2 className="font-bold text-xl mb-3">Pain points</h2>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
+      <div className="bg-[var(--color-card-bg-light)] dark:bg-[var(--color-card-bg-dark)] p-8 rounded-lg shadow-md">
+        <h2 className="font-bold text-3xl mb-3 text-[var(--color-card-h2-light)] dark:text-[var(--color-card-h2-dark)]">
+          Pain points
+        </h2>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
       </div>
-      <div className="bg-blue-200 dark:bg-blue-400 p-8 rounded-lg shadow-md">
-      <h1 className="font-bold text-xl mb-3">Goals</h1>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
-        <p>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, magni expedita!</p>
+      <div className="bg-[var(--color-card-bg-light)] dark:bg-[var(--color-card-bg-dark)] p-8 rounded-lg shadow-md">
+        <h1 className="font-bold text-3xl mb-3 text-[var(--color-card-h2-light)] dark:text-[var(--color-card-h2-dark)]">
+          Goals
+        </h1>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
+        <p className="text-[var(--color-card-paragraph-light)] dark:text-[var(--color-card-paragraph-dark)]">
+          - Lorem ipsum dolor sit amet, fgefw consectetur adipiscing elit. Euchue
+        </p>
       </div>
     </div>
   );
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,12 @@
+import { useDarkMode } from "../hooks/useDarkMode";
 import ThemeToggle from "~/components/ThemeToggle";
 
 function Header() {
   return (
     <header className="flex items-center justify-between p-4 shadow-md w-full">
       <div className="w-[500px] max-w-[100vw] p-4">
-        <h1 className="text-5xl text-black dark:text-white">
-          <span className="underline decoration-blue-500 dark:decoration-blue-300 text-black dark:text-white">
+        <h1 className="text-5xl text-[var(--color-main-header-light)] dark:text-[var(--color-main-header-dark)]">
+          <span className="underline decoration-[var(--color-primary-light)] dark:decoration-[var(--color-primary-dark)]">
             User
           </span>{" "}
           Persona
